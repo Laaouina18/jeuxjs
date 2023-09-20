f1501fc313d4b3184dd5ee379550c42947e1cd97
Laaouina18 <laaouinanouhaila2019@gmail.com>
2023-09-20 15:20:49 +0100

    random des photos

---
+++ script.js
@@ -0,0 +1,20 @@
+const startButton = document.getElementById("startButton");
+
+    const moleImages = document.querySelectorAll(".mole");
+    let gameStarted = false;
+    function showRandomMole() {
+      moleImages.forEach((img) => {
+        img.style.display = "none";
+      });
+
+      const randomIndex = Math.floor(Math.random() * moleImages.length);
+      const randomMole = moleImages[randomIndex];
+      
+      randomMole.style.display = "block"; 
+    }
+    startButton.addEventListener("click", () => {
+      if (!gameStarted) {
+        gameStarted = true;
+        setInterval(showRandomMole, 1000); 
+      }
+    });
