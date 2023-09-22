cbc109c75e5909663f7bebf95ea556c07d0b9b30
Laaouina18 <laaouinanouhaila2019@gmail.com>
2023-09-22 12:22:57 +0100

    mettre à jour

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-const startButton = document.getElementById("startButton");
+const start = document.getElementById("startButton");
+const moleImages = document.querySelectorAll(".mole");
+let gameStarted = false;
+let activeMole = [];
 
-    const moleImages = document.querySelectorAll(".mole");
-    let gameStarted = false;
-    function showRandomMole() {
-      moleImages.forEach((img) => {
-        img.style.display = "none";
-      });
 
-      const randomIndex = Math.floor(Math.random() * moleImages.length);
-      const randomMole = moleImages[randomIndex];
-      
-      randomMole.style.display = "block"; 
-    }
-    startButton.addEventListener("click", () => {
-      if (!gameStarted) {
-        gameStarted = true;
-        setInterval(showRandomMole, 2000); 
-      }
-    });
+function voirPhoto() {
+  moleImages.forEach((img) => {
+    img.style.display = "none";
+  });
+  const randomIndex = Math.floor(Math.random() * moleImages.length);
+  const randomMole = moleImages[randomIndex];
+
+  activeMole.push(randomIndex);
+
+  if (activeMole.includes(randomIndex)) {
+    randomMole.setAttribute("src", "./mole-game/hungry.png")
+  }
+
+  randomMole.style.display = "block";
+  
+  setTimeout(function () {
+    randomMole.setAttribute("src", "./mole-game/sad.png")
+    
+
+  }, 1400);
+  setTimeout(function () {
+    randomMole.setAttribute("src", "./mole-game/leaving.png")
+  }, 1800);
+  
+  
+}
+start.addEventListener("click", () => {
+  if (!gameStarted) {
+    gameStarted = true;
+    setInterval(voirPhoto, 2000);
+    add = document.getElementById("mybody");
+    add.classList.add("cursor");
+  }
+});
